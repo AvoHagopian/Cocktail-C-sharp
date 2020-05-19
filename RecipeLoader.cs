9f97084768996b06b9def0fe5c7eb82d3a18b61f
AvoHagopian <hagopian.avo@gmail.com>
2020-05-18 17:51:08 -0700

    Finished class definitions

---
+++ RecipeLoader.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections;
+
+namespace RecipeApplication
+{
+    class Recipe
+    {
+        private int ID;
+        private string name;
+        private ArrayList<Ingredient> ingredientList;
+        private string prepStyle;
+        private string iceStyle;
+        private string garnish;
+        private string glass;
+        private string instructions;
+
+        public Recipe(int id, string na, ArrayList<Ingredient> il, string pr, string ic, string ga, string gl, string i)
+        {
+            ID = id;
+            name = na;
+            ingredientList = il;
+            prepStyle = pr;
+            iceStyle = ic;
+            garnish = ga;
+            glass = gl;
+            instructions = i;
+        }
+        public Recipe()
+        {
+            ID = 0;
+            name = "";
+            ingredientList.Clear();
+            prepStyle = "";
+            iceStyle = "";
+            garnish = "";
+            glass = "";
+            instructions = "";
+        }
+
+        public static bool operator < (Recipe lhs, Recipe rhs)
+        {
+            if(Compare(lhs.getName(), rhs.getName()) < 0)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator > (Recipe lhs, Recipe rhs)
+        {
+            if(Compare(lhs.getName(), rhs.getName()) > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public int getID() {    return ID;  }
+        public string getName() {    return name;  }
+        public ArrayList<Ingredient> getIngredientList() {return ingredientList;  }
+        public string getPrepStyle() {    return prepStyle;    }
+        public string getIceStyle() {    return iceStyle;  }
+        public string getGarnish() {    return garnish;    }
+        public string getGlass() {    return glass;    }
+        public string getInstructions() {    return instructions;  }
+
+        public void setName(string n) { name = n;   }
+        public void setIngredientList(ArrayList<Ingredient> i) {    ingredientList = i; }
+        public void setPrepStyle(string p) {    prepStyle = p;  }
+        public void setIceStyle(string i) { iceStyle = i;   }
+        public void setGarnish(string g) {  garnish = g;    }
+        public void setGlass(string g) {    glass = g;  }
+        public void setInstructions(string i) { instructions = i;   }
+    }
+
+    class Ingredient
+    {
+        private double amount;
+        private string unit;
+        private string ingredient;
+        
+        public Ingredient(double a, string u, string i)
+        {
+            amount = a;
+            unit = u;
+            ingredient = i;
+        }
+        public Ingredient()
+        {
+            amount = 0;
+            unit = "NULL";
+            ingredient = "";
+        }
+
+        public double getAmount(){ return amount;  }
+        public string getUnit(){   return unit;    }
+        public string getIngredient(){ return ingredient;  }
+
+        public void setAmount(double a){   amount = a; }
+        public void setUnit(string u){ unit = u;   }
+        public void setIngredient(string i){   ingredient = i; }
+
+        public int setString(string i)
+        {
+            string[] parts = i.Split(' ', 3);
+            amount = ToString(parts[0]);
+            try
+            {
+                amount = Convert.ToDouble(parts[0]);
+            }
+            catch (FormatException)
+            {
+                return 1;
+            }
+            unit = parts[1];
+            ingredient = parts[2];
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0:F} {1} {2}", amount, unit, ingredient);
+        }
+    }
+
+    class RecipeLoader
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yo");
+        }
+    }
+}
